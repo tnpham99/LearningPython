c8f7ab0062305d35fcb245d31e3d419d1e83bfc9
tnpham99 <tnpham0112@yahoo.com>
2022-11-18 18:11:05 -0500

    update class inheritance exercises

--- HomeLearning/Day3/oop_exercises.py
+++ HomeLearning/Day3/oop_exercises.py
@@ -8,8 +8,8 @@ class Vehicle():
         self.mileage = mileage
         self.capacity = capacity
 
-    def get_seating_capacity(self):
-        return f'The seating capacity of a {self.name} is {self.capacity} passengers.'
+    def set_seating_capacity(self, capacity):
+        return f'The seating capacity of a {self.name} is {capacity} passengers.'
 
     def get_fare(self):
         return self.capacity*100
@@ -18,24 +18,25 @@ class Vehicle():
 #     pass
 
 class Bus(Vehicle):
-    def __init__(self, name, max_speed, mileage, capacity=50) -> None:
+    def __init__(self, name, max_speed, mileage, capacity) -> None:
         super().__init__(name, max_speed, mileage, capacity)
 
-    def get_seating_capacity(self):
-        return super().get_seating_capacity()
+    def set_seating_capacity(self, capacity=50):
+        return super().set_seating_capacity(capacity)
     
     def get_fare(self):
         final_amount = super().get_fare() + (.1*super().get_fare())
         return final_amount
+
 class Car(Vehicle):
-    def __init__(self, name, max_speed, mileage, capacity) -> None:
+    def __init__(self, name, max_speed, mileage, capacity=None) -> None:
         super().__init__(name, max_speed, mileage, capacity)
 
-bus1 = Bus('School Volvo', 180, 12)
-car1 = Car('Audi Q5', 240, 18, 4)
+bus1 = Bus('School Volvo', 180, 12, 50)
+car1 = Car('Audi Q5', 240, 18)
 
 print('Color:', bus1.color, '\nVehicle Name:', bus1.name, '\nSpeed:', bus1.max_speed, '\nMileage:', bus1.mileage)
-print(bus1.get_seating_capacity())
+print(bus1.set_seating_capacity())
 
 print('Color:', car1.color, '\nVehicle Name:', car1.name, '\nSpeed:', car1.max_speed, '\nMileage:', car1.mileage)
 
